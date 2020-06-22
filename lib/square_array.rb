ec22a4de58102e3f28a771e1cac93734d1eea75d
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-22 22:51:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,6 @@
 def square_array(array)
-  array = [1,2,3]
-  new_array = [9, 10, 16, 25]
-   counter = 0
-   
-  while counter < numbers.length()
-  new_array.push(array[counter] ** array[counter])
-  counter += 1
-  
-  end 
-  return new_array
+  Enumerator.new do |y|
+    array.each { |e| y << e ** 2 }    
+  end
+  .take(array.length)
 end
-  
