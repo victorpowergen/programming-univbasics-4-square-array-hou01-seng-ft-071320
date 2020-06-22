d0746f1bcedc5904aabaece0541764fa265c1c89
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-22 22:44:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  array = [1,2,3]
+  new_array = [9, 10, 16, 25]
+   counter = 0
+   
+  while counter < numbers.length()
+  new_array.push(array[counter] ** array[counter])
+  counter += 1
+  
+  end 
+  return new_array
 end
+  
