efc49d5ea44965e4c400577d609d46a83d6725ef
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-22 23:04:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   Enumerator.new do |y|
-    array.each { |e| y << e ** 2 }  
-    array = [1, 2, 3]
+    array.each { |e| y << e ** [1, 2, 3] }  
+    
   end
   .take(array.length)
 end
